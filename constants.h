e3a13d38a99fa0199953c866fd6d75a3cd838c6b
W0L1D <ahdoufwalid@gmail.com>
2022-04-08 14:44:06 +0000

    constants.h added

---
+++ constants.h
@@ -0,0 +1,16 @@
+//
+// Created by W0L1D on 4/8/2022.
+//
+
+#ifndef MAIN_C_CONSTANTS_H
+#define MAIN_C_CONSTANTS_H
+
+#define DATA_KEY_INDIVIDU "individu"
+#define DATA_KEY_LIST_INDIVIDU "ListIndividus"
+#define DATA_KEY_LIST_VIRUS "listVirus"
+
+#define MAXrow 70
+#define MAXcol 40
+
+
+#endif //MAIN_C_CONSTANTS_H
