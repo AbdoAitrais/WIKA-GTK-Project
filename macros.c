0adc460549c94d72d18708477353c51bdcbdf882
irsabIsm <92953280+irsabIsm@users.noreply.github.com>
2022-04-29 16:28:08 +0200

    Update macros.c

--- macros.c
+++ macros.c
@@ -9,6 +9,145 @@
 
 
 
+    void AgePerson(Individu P,float *val)
+{
+     switch(P.categorie)
+    {
+        case AGE_KIDS :
+            *val += 0.5;
+            break;
+        case AGE_TEENS :
+            *val +=0.3;
+            break;
+        case AGE_YOUTH :
+            *val+=0.7;
+            break;
+        case AGE_ADULT :
+            *val++;
+            break;
+        case AGE_OLD :
+            *val+=0.1;
+        default: *val = 0.0;
+    }
+
+}
+void LaGenetique(Individu P,float* val)
+{
+    switch(P.health.genetic)
+    {
+        case GENETIQUEMENT_FAIBLE:
+            *val+=0.3;
+            break;
+        case GENETIQUEMENT_FRAGILE:
+            *val+=0.4;
+            break;
+        case GENETIQUEMENT_MOYEN:
+            *val+=0.6;
+            break;
+        case GENETIQUEMENT_FORT:
+            *val++;
+    }
+}
+void LaTension(Individu P,float *val)
+{
+    switch(P.health.tension)
+    {
+        case ARTERIELLE_NORMAL:
+            *val+=0.5;
+            break;
+        case ARTERIELLE_HYPERTENDU:
+            *val+=0.7;
+            break;
+        case ARTERIELLE_HYPERTENSION_FORTE:
+            *val++;
+    }
+}
+void LaDiabete(Individu P,float* val)
+{
+    switch(P.health.diabete)
+    {
+        case DIABETE_NORMAL:
+            *val+=0.5;
+            break;
+        case DIABETE_MODERE:
+            *val+=0.8;
+            break;
+        case DIABETE_AVANCE:
+            *val++;
+    }
+}
+void LaCardiaque(Individu P,float* val)
+{
+    switch(P.health.cardiac)
+    {
+        case CARDIAQUE_NORMAL:
+            *val++;
+            break;
+        case CARDIAQUE_MALADE:
+            *val+=0.4;
+            break;
+        case CARDIAQUE_SEVERE:
+            *val+=0.1;
+    }
+}
+
+
+void LaPoumons(Individu P,float* val)
+{
+    switch(P.health.poumons)
+    {
+        case POUMONS_SEIN:
+            *val++;
+            break;
+        case POUMONS_MALADE:
+            *val+=0.5;
+            break;
+        case POUMONS_GRAVE:
+            *val+=0.2;
+    }
+}
+
+
+float etatSanteIndivdu(Individu P)
+{
+    float etat = 0.0;
+    AgePerson(P,&etat);
+    LaGenetique(P,&etat);
+    LaDiabete(P,&etat);
+    LaCardiaque(P,&etat);
+    LaPoumons(P,&etat);
+    LaTension(P,&etat);
+    return ((float)etat);
+}
+
+
+int includeCercle(GtkGrid* Tab,Coord pos,Virus V)
+{
+    int i,j;
+        for(i=(pos.x-V.cercleDeContam);i<(pos.x+V.cercleDeContam);i++)/// on pacrourt la cercle de contamination de première ligne jusqu'à le dernière
+        {
+            if( i<0 || i>=MAXrow)///    if it is out of the table(grid)
+                continue;
+            for(j=(pos.y-V.cercleDeContam);j<(pos.y+V.cercleDeContam);j++)///on pacrourt la cercle de contamination de premier colonne jusqu'à la dernière
+                {
+                        if( j< 0 || j>=MAXcol)/// if it is out of the table(grid)
+                                continue;
+                          GtkWidget* image = gtk_grid_get_child_at(Tab,i,j);
+                        Individu* PersonExiste = (Individu*)g_object_get_data(image,DATA_KEY_INDIVIDU);
+                            if(PersonExiste)
+                }
+        }
+}
+
+
+
+/// check this out
+
+
+
+
+
+
 /*********** project functions ****************/
 
 void loadCSS(GtkWidget *window)
