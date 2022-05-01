6a302a1467a0860a408776870d681c2217fd85e0
irsabIsm <92953280+irsabIsm@users.noreply.github.com>
2022-05-02 01:43:40 +0200

    Update UI_individu_macros.h

--- by Abderrahman/UI_individu_macros.h
+++ by Abderrahman/UI_individu_macros.h
@@ -11,6 +11,120 @@
 #include "../by ismail/contaminate_utils.h"
 #include "dialog_macro.h"
 
+
+/******  enum to caracteristiques   *************/
+
+
+
+
+gchar* associerGender(Individu* indiv)
+{
+    switch(indiv->gender)
+    {
+        case GENRE_MALE:
+            return (gchar*)(" Homme");
+        case GENRE_FEMALE:
+            return (gchar*)(" Femme");
+    }
+
+}
+
+
+gchar* associerGenetique(Individu* indiv)
+{
+    switch(indiv->health.genetic)
+    {
+        case GENETIQUEMENT_FAIBLE:
+            return (gchar*)(" Faible");
+        case GENETIQUEMENT_FRAGILE:
+            return (gchar*)(" Fragile");
+        case GENETIQUEMENT_MOYEN:
+            return (gchar*)(" Moyen");
+        case GENETIQUEMENT_FORT:
+            return (gchar*)(" FORT");
+    }
+}
+gchar* associerTension(Individu* indiv)
+{
+    switch(indiv->health.tension)
+    {
+        case ARTERIELLE_NORMAL:
+            return (gchar*)(" Normal");
+        case ARTERIELLE_HYPERTENDU:
+            return (gchar*)(" Hypertendu");
+        case ARTERIELLE_HYPERTENSION_FORTE:
+            return (gchar*)(" Hypertendu forte");
+    }
+}
+
+gchar* associerDiabete(Individu* indiv)
+{
+    switch(indiv->health.diabete)
+    {
+        case DIABETE_NORMAL:
+            return (gchar*)(" Normal");
+        case DIABETE_MODERE:
+            return (gchar*)(" Modere");
+        case DIABETE_AVANCE:
+            return (gchar*)(" Avance");
+    }
+}
+
+gchar* associerCardiaque(Individu* indiv)
+{
+    switch(indiv->health.cardiac)
+    {
+        case CARDIAQUE_NORMAL:
+            return (gchar*)(" Normal");
+        case CARDIAQUE_MALADE:
+            return (gchar*)(" Malade");
+        case CARDIAQUE_SEVERE:
+            return (gchar*)(" Severe");
+    }
+}
+
+gchar* associerPoumons(Individu* indiv)
+{
+    switch(indiv->health.poumons)
+    {
+        case POUMONS_SEIN:
+            return (gchar*)(" Sein");
+        case POUMONS_MALADE:
+            return (gchar*)(" Malade");
+        case POUMONS_GRAVE:
+            return (gchar*)(" Grave");
+    }
+}
+
+
+gchar* associerAge(Individu* indiv)
+{
+    switch(indiv->categorie)
+    {
+        case AGE_KIDS:
+            return (gchar*)(" enfant");
+        case AGE_TEENS:
+            return (gchar*)(" adolescent");
+        case AGE_YOUTH:
+            return (gchar*)(" Jeune");
+        case AGE_ADULT:
+            return (gchar*)(" Adulte");
+        case AGE_OLD:
+            return (gchar*)(" vieux");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 /************* Individu ***************/
 
 void afficher_VirusList(GList *VirusList);
